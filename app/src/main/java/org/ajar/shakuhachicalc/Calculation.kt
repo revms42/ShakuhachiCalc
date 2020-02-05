da00e32bca9e6fd9da4860c746b19c155ed42282
Matt Stockbridge <Matthew.Stockbridge@sap.com>
2020-02-05 12:12:40 -0800

    Making some fixes around how the dark values are calculated and other bug fixes. Added a field that gives the closest note to the principle frequency of the flute.

--- app/src/main/java/org/ajar/shakuhachicalc/Calculation.kt
+++ app/src/main/java/org/ajar/shakuhachicalc/Calculation.kt
@@ -5,6 +5,65 @@ import kotlin.math.pow
 /**
  * Derived from https://www.scribd.com/doc/178200652/Shakuhaki-Design-Excellent-Guide-to-Flutes
  */
+enum class Key(val rep: String) {
+    A("An"),
+    B_F("Bnb"),
+    B("Bn"),
+    C("Cn"),
+    C_S("Cn#"),
+    D("Dn"),
+    E_F("Enb"),
+    E("En"),
+    F("Fn"),
+    F_S("Fn#"),
+    G("Gn"),
+    G_S("Gn#");
+
+    fun step(dir: Int) : Key {
+        return when(dir) {
+            0 -> this
+            in 1..12 -> {
+                when(this) {
+                    A -> G_S.step(dir - 1)
+                    else -> values()[this.ordinal - 1].step(dir - 1)
+                }
+            }
+            in -1 downTo -12 -> {
+                when(this) {
+                    G_S -> A.step(dir + 1)
+                    else -> values()[this.ordinal + 1].step(dir + 1)
+                }
+            }
+            else -> throw NumberFormatException("Wrong dimension $dir")
+        }
+    }
+
+    companion object {
+        private const val a4 = 440.0 // Hz
+        private val aConst = (2.0).pow(1.0/12.0)
+
+        fun find(frequency: Double): String {
+            var aNumber = 0.0
+            if(frequency > a4) {
+                while(a4 * aConst.pow(aNumber) < frequency) {
+                    aNumber += 1.0
+                }
+            } else {
+                while(a4 * aConst.pow(aNumber) > frequency) {
+                    aNumber -= 1.0
+                }
+            }
+
+            val octaves = aNumber.toInt() / 12
+            val steps = aNumber.toInt() % 12
+
+            val nNumber = 4 + octaves
+
+            return A.step(steps).rep.replace("n", "$nNumber")
+        }
+    }
+}
+
 interface CalculationDesc {
     fun lengthFromBore(boreDia: Double) : Double
     fun boreFromLength(length: Double): Double
@@ -19,7 +78,7 @@ sealed class Calculation : CalculationDesc {
     }
 
     object Dark : Calculation() {
-        override fun lengthFromBore(boreDia: Double) : Double = boreDia.pow(sixFifths) * (2.0 * Math.PI.pow(sixFifths))
+        override fun lengthFromBore(boreDia: Double) : Double = boreDia.pow(sixFifths) * Math.PI.pow(2.0 * sixFifths)
         override fun boreFromLength(length: Double): Double = length.pow(fiveSixths) / Math.PI.pow(2.0)
         override fun holeFromLength(length: Double) : Double = length.pow(twoThirds) / (2.0 * Math.PI)
     }
@@ -36,6 +95,7 @@ sealed class Calculation : CalculationDesc {
         private const val bottomHoleConst = 121.0
         private const val holeToHoleConst = 10.0
         private const val hole4to5Const = 36.0
+        private const val velocityOfSoundInAir = 350000.0 // mm/s
 
         fun computeEAR(boreDia: Double, length: Double): Double = (earConst * length.pow(fiveSixths)) / boreDia
         fun computeBoreFromEAR(ear: Double, length: Double) : Double = (earConst * length.pow(fiveSixths)) / ear
@@ -56,5 +116,10 @@ sealed class Calculation : CalculationDesc {
         fun utaguchiWidth(boreDia: Double) : Double = (boreDia * Math.PI) / 4.0
         fun utaguchiHeight(boreDia: Double) : Double = utaguchiWidth(boreDia) / 5.0
         fun holeFromBore(boreDia: Double) : Double = boreDia.pow(fourFifths)
+        fun frequencyFromLength(length: Double) : Double = velocityOfSoundInAir/(2.0 * length)
+
+        fun findKey(length: Double) : String {
+            return Key.find(frequencyFromLength(length))
+        }
     }
 }
--- app/src/main/java/org/ajar/shakuhachicalc/MeasurementViewModel.kt
+++ app/src/main/java/org/ajar/shakuhachicalc/MeasurementViewModel.kt
@@ -1,10 +1,11 @@
 package org.ajar.shakuhachicalc
 
 import androidx.lifecycle.*
+import kotlin.math.roundToLong
 
-class DoubleTransformBinding {
+class NumberTransformBinding {
     val text = MutableLiveData<String>()
-    val number = Transformations.map(text) { it.toDouble() }
+    val number = Transformations.map(text) { if(it.isNotEmpty()) it.toDouble().roundToLong() else 0 }
 
     init {
         number.observeForever { if(it.toString() != text.value) text.value = it.toString() }
@@ -12,13 +13,14 @@ class DoubleTransformBinding {
 }
 
 class MeasurementViewModel : ViewModel() {
-    val topBore = DoubleTransformBinding()
-    val bottomBore = DoubleTransformBinding()
-    val length = DoubleTransformBinding()
+    val topBore = NumberTransformBinding()
+    val bottomBore = NumberTransformBinding()
+    val length = NumberTransformBinding()
     val utaguchiWidth: LiveData<String>
     val utaguchiHeight: LiveData<String>
     val fingerHoleDiameter: LiveData<String>
     val earRatio = MediatorLiveData<String>()
+    val nearestNote: LiveData<String>
     val holeOne: LiveData<String>
     val holeTwo: LiveData<String>
     val holeThree: LiveData<String>
@@ -29,37 +31,38 @@ class MeasurementViewModel : ViewModel() {
 
     var calculation: CalculationDesc = Calculation.Bright
 
-    private val averageBore: Double
+    private val averageBore: Long
         get() {
-            val sum = (topBore.number.value?: 0.0) + (bottomBore.number.value?: 0.0)
-            return if(sum != 0.0) {
-                sum / 2.0
+            val sum = (topBore.number.value?: 0L) + (bottomBore.number.value?: 0L)
+            return if(sum != 0L) {
+                sum / 2L
             } else {
-                0.0
+                0L
             }
         }
 
     init {
         isBright.observeForever {
-            calculation = if(it) Calculation.Bright else Calculation.Dark
-            length.text.value = calculation.lengthFromBore(averageBore).toString()
+            calculation = if(it) Calculation.Dark else Calculation.Bright
+            length.text.value = calculation.lengthFromBore(averageBore.toDouble()).toString()
         }
-        utaguchiWidth = Transformations.map(topBore.number) { Calculation.utaguchiWidth(it).toString() }
-        utaguchiHeight = Transformations.map(topBore.number) { Calculation.utaguchiHeight(it).toString() }
-        fingerHoleDiameter = Transformations.map(length.number) { calculation.holeFromLength(it).toString() }
+        utaguchiWidth = Transformations.map(topBore.number) { Calculation.utaguchiWidth(it.toDouble()).toString() }
+        utaguchiHeight = Transformations.map(topBore.number) { Calculation.utaguchiHeight(it.toDouble()).toString() }
+        fingerHoleDiameter = Transformations.map(length.number) { calculation.holeFromLength(it.toDouble()).toString() }
         earRatio.addSource(length.number) {
-            earRatio.value = Calculation.computeEAR(averageBore, it).toString()
+            earRatio.value = Calculation.computeEAR(averageBore.toDouble(), it.toDouble()).toString()
         }
         earRatio.addSource(topBore.number) {
-            earRatio.value = Calculation.computeEAR(averageBore, length.number.value?: 0.0).toString()
+            earRatio.value = Calculation.computeEAR(averageBore.toDouble(), length.number.value?.toDouble()?: 0.0).toString()
         }
         earRatio.addSource(bottomBore.number) {
-            earRatio.value = Calculation.computeEAR(averageBore, length.number.value?: 0.0).toString()
+            earRatio.value = Calculation.computeEAR(averageBore.toDouble(), length.number.value?.toDouble()?: 0.0).toString()
         }
-        holeOne = Transformations.map(length.number) { Calculation.computeHolePosition(1, it).toString() }
-        holeTwo = Transformations.map(length.number) { Calculation.computeHolePosition(2, it).toString() }
-        holeThree = Transformations.map(length.number) { Calculation.computeHolePosition(3, it).toString() }
-        holeFour = Transformations.map(length.number) { Calculation.computeHolePosition(4, it).toString() }
-        holeFive = Transformations.map(length.number) { Calculation.computeHolePosition(5, it).toString() }
+        nearestNote = Transformations.map(length.number) { Calculation.findKey(it.toDouble()) }
+        holeOne = Transformations.map(length.number) { Calculation.computeHolePosition(1, it.toDouble()).toString() }
+        holeTwo = Transformations.map(length.number) { Calculation.computeHolePosition(2, it.toDouble()).toString() }
+        holeThree = Transformations.map(length.number) { Calculation.computeHolePosition(3, it.toDouble()).toString() }
+        holeFour = Transformations.map(length.number) { Calculation.computeHolePosition(4, it.toDouble()).toString() }
+        holeFive = Transformations.map(length.number) { Calculation.computeHolePosition(5, it.toDouble()).toString() }
     }
 }
